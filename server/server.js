68d49e639f416bcf48b7b30c894ef7aec7b17835
Designer-space <123vinaymaurya@gmail.com>
2024-07-05 11:26:31 +0530

    fixed error

--- server/server.js
+++ server/server.js
@@ -17,8 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(notFound)
-app.use(errorHandler)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  app.use(notFound)
+  app.use(errorHandler)
+})
 app.use("/api/users", userRoutes)
 
 const __dirname = path.resolve()
